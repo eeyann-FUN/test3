f187514f541b98e1063c35a0e3b7f20985299b16
Tatsuika <tatu3939104@gmail.com>
2018-12-21 20:30:46 +0900

    a

---
+++ test3/SubViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SubViewController.swift
+//  test3
+//
+//  Created by 猪飼　立晟 on 2018/12/21.
+//  Copyright © 2018年 Tatsuika. All rights reserved.
+//
+
+import UIKit
+
+class SubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
